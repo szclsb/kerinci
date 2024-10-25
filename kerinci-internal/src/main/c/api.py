4572e1f65783126f925131c359e467c2132a2c21
Claudio Seitz <szclsb@gmail.com>
2024-10-25 12:52:41 +0200

    moved dll definition generation to python script, called by cmake

---
+++ kerinci-internal/src/main/c/api.py
@@ -0,0 +1,77 @@
+import os
+import shutil
+import argparse
+import re
+
+def main():
+    parser = argparse.ArgumentParser(prog='generate glfw/vulkan dll spec')
+    parser.add_argument('-t', '--target', help='target directory to write the source file, header file will be written to $target/include')
+    parser.add_argument('-f', '--function', help='path to file containing function names as filter')
+    parser.add_argument('-s', '--sources', help='path of source header files to scan', nargs='+')
+    parser.add_argument('-p', '--prefix', help='wrapper function prefix')
+    parser.add_argument('-e', '--exclusions', help='keywords to exclude form generated functions', nargs='*')
+
+    args = parser.parse_args()
+    print('api.py ' + str(args))
+
+    regex = re.compile('.*\s(\w+)\(([a-zA-Z0-9_\r\n\t\f\v \,\*]*)\);')
+
+    functions = set()
+    functionsFile = open(os.path.join(args.function), 'r')
+    for line in functionsFile:
+        function = line.strip()
+        if function:
+            print('-- ' + function)
+            functions.add(function)
+
+    tmpHeaderPath = os.path.join(args.target, 'api.h.tmp')
+    tmpSourcePath = os.path.join(args.target, 'api.c.tmp')
+
+    tmpHeaderFile = open(tmpHeaderPath, 'w')
+    tmpSourceFile = open(tmpSourcePath, 'w')
+
+    tmpHeaderFile.write("""
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include "GLFW/glfw3.h"
+
+""")
+    tmpSourceFile.write("""
+#include "api.h"
+
+""")
+
+    for source in args.sources:
+        print('scanning ' + source)
+        sourceFile = open(source, 'r')
+        for match in regex.finditer(sourceFile.read(), re.MULTILINE):
+            function = match.group(1)
+            if function in functions:
+                headerFunctionDefinition = match.group(0).replace(function, args.prefix + function).replace('(void)', '()')
+                for exclusion in args.exclusions:
+                    headerFunctionDefinition = headerFunctionDefinition.replace(exclusion, '')
+                headerFunctionDefinition = headerFunctionDefinition.strip()
+                tmpHeaderFile.write(f"""
+__declspec(dllexport) {headerFunctionDefinition}""")
+                params = filter(filterArg, map(extractArg, match.group(2).split(",")))
+                tmpSourceFile.write(headerFunctionDefinition.replace(";", f"""{{
+    {'' if headerFunctionDefinition.startswith('void') else 'return '}{function}({', '.join(params)});
+}}
+"""))
+
+    tmpHeaderFile.close()
+    tmpSourceFile.close()
+
+    shutil.move(tmpHeaderPath, os.path.join(args.target, 'include', 'api.h'))
+    shutil.move(tmpSourcePath, os.path.join(args.target, 'api.c'))
+
+def filterArg(arg):
+    return arg != 'void' and arg
+
+def extractArg(arg):
+    i = arg.rfind(' ')
+    return arg[i+1:]
+
+if __name__ == '__main__':
+    main()
--- native-lib-plugin/src/main/java/ch/szclsb/maven/plugins/NativeLibMojo.java
+++ native-lib-plugin/src/main/java/ch/szclsb/maven/plugins/NativeLibMojo.java
@@ -5,20 +5,10 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.nio.file.StandardOpenOption;
 import java.util.*;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 @Mojo(name = "native-lib")
 public class NativeLibMojo extends AbstractCommandProcessMojo {
-    @Parameter(property = "wrappers")
-    private Collection<Wrapper> wrappers;
-
     @Parameter(property = "windows")
     private boolean windows;
     @Parameter(property = "nativePath", required = true)
@@ -33,104 +23,8 @@ public class NativeLibMojo extends AbstractCommandProcessMojo {
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        generateWrappers();
         generateNativeLib();
         generateNativeHandlers();
-    }
-
-    private void generateWrappers() throws MojoExecutionException {
-        getLog().info("start generating wrappers");
-        var cwd = Paths.get(getWorkingDirectory().toURI());
-        var nativeWorkingDir = cwd.resolve(nativePath);
-        var pattern = Pattern.compile(".*\\s(\\w+)\\(([a-zA-Z0-9_\\r\\n\\t\\f\\v \\,\\*]*)\\);");
-        for (var wrapper : wrappers) {
-            getLog().info("Generating wrapper for " + wrapper.getName());
-            try {
-                var functions = new HashSet<String>();
-                try (var stream = Files.lines(cwd.resolve(wrapper.getFunctionListPath()))) {
-                    stream.filter(line -> !line.isEmpty()).forEach(functions::add);
-                }
-                var headerFile = nativeWorkingDir.resolve("include/" + wrapper.getName() + ".h");
-                var sourceFile = nativeWorkingDir.resolve(wrapper.getName() + ".c");
-
-                var tmpHeaderFile = Files.createTempFile(cwd, null, null);
-                var tmpSourceFile = Files.createTempFile(cwd, null, null);
-                try (var headerWriter = Files.newBufferedWriter(tmpHeaderFile, StandardOpenOption.WRITE,
-                        StandardOpenOption.CREATE);
-                     var sourceWriter = Files.newBufferedWriter(tmpSourceFile, StandardOpenOption.WRITE,
-                             StandardOpenOption.CREATE)) {
-                    headerWriter.write("""
-                            #pragma once
-                            
-                            """);
-                    for (var define : wrapper.getDefines()) {
-                        headerWriter.write("""
-                                #define %s
-                                """.formatted(define));
-                    }
-                    for (var define : wrapper.getIncludes()) {
-                        headerWriter.write("""
-                                #include %s
-                                """.formatted(define));
-                    }
-                    headerWriter.newLine();
-
-                    sourceWriter.write("""
-                            #include \"%s.h\"
-                            
-                            """.formatted(wrapper.getName()));
-
-                    for (var sourceHeader : wrapper.getHeaderFiles()) {
-                        try (var scanner = new Scanner(Paths.get(sourceHeader))) {
-                            var matches = scanner.findAll(pattern).toList();
-                            for (var match : matches) {
-                                var function = match.group(1);
-                                if (functions.contains(function)) {
-                                    var definition = match.group(0);
-
-                                    var headerFunctionDefinition = definition.replace(function, wrapper.getFunctionPrefix() + function);
-                                    headerFunctionDefinition = headerFunctionDefinition.replace("(void);", "();");
-                                    for (var exclusion : wrapper.getExcludes()) {
-                                        headerFunctionDefinition = headerFunctionDefinition.replaceAll(exclusion, "");
-                                    }
-                                    headerFunctionDefinition = headerFunctionDefinition.trim();
-                                    headerWriter.write("""
-                                            __declspec(dllexport) %s
-                                            """.formatted(headerFunctionDefinition));
-
-                                    var argLines = match.group(2);
-                                    var args = Arrays.stream(argLines.split(","))
-                                            .map(str -> {
-                                                var i = str.lastIndexOf(' ');
-                                                return str.substring(i + 1);
-                                            })
-                                            .filter(str -> !str.isEmpty() && !str.equals("void"))
-                                            .collect(Collectors.joining(", "));
-                                    var sourceFunctionDefinition = headerFunctionDefinition.replace(";", """
-                                            {
-                                                %s%s(%s);
-                                            }
-                                            """.formatted(headerFunctionDefinition.startsWith("void") ? "" : "return ", function, args));
-                                    sourceWriter.write(sourceFunctionDefinition);
-
-                                }
-                            }
-                        }
-                    }
-                    headerWriter.flush();
-                    sourceWriter.flush();
-
-                    Files.move(tmpHeaderFile, headerFile, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.ATOMIC_MOVE);
-                    Files.move(tmpSourceFile, sourceFile, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.ATOMIC_MOVE);
-                } finally {
-                    Files.deleteIfExists(tmpHeaderFile);
-                    Files.deleteIfExists(tmpSourceFile);
-                }
-            } catch (Exception e) {
-                getLog().error(e);
-            }
-        }
-        getLog().info("finished generating wrappers");
     }
 
     private void generateNativeLib() throws MojoExecutionException {
