3b598006571d0ab6dd598d3a9aaa60345d3d1db8
szclsb <szclsb@gmail.com>
2025-03-08 12:04:50 +0100

    added missing Runtime class

---
+++ kerinci-internal/src/main/java/ch/szclsb/kerinci/internal/Runtime.java
@@ -0,0 +1,33 @@
+package ch.szclsb.kerinci.internal;
+
+import org.reflections.Reflections;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Runtime {
+    private Runtime () {}
+
+    private static final Map<Class<?>, Map<Integer, ?>> typeConstants = initHasValueTypes();
+
+    private static Map<Class<?>, Map<Integer, ?>> initHasValueTypes() {
+        var result = new HashMap<Class<?>, Map<Integer, ?>>();
+        var reflections = new Reflections("ch.szclsb.kerinci.api");
+        reflections.getSubTypesOf(HasValue.class).forEach(nativeEnum -> {
+            if (nativeEnum.isEnum()) {
+                var constants = new TreeMap<Integer, Object>();
+                for (var c : nativeEnum.getEnumConstants()) {
+                    constants.putIfAbsent(c.getValue(), c);
+                }
+                result.put(nativeEnum, constants);
+            }
+        });
+        return result;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends HasValue> T getConstOfValue(int value, Class<T> enumClass) {
+        return (T) typeConstants.get(enumClass).get(value);
+    }
+}
